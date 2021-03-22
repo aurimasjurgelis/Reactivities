22474101a9b0472c40f82c07f59dc6a787d344b3
aurimasjurgelis <dev.aurimas@gmail.com>
2021-03-22 21:23:36 +0200

    123

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,65 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './styles.css';
+import axios from 'axios';
+import { Container, Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+
+function App() {
+
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+
+  const [editMode, setEditMode] = useState(false);
+
+  useEffect(() => {
+    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
+      setActivities(response.data);
+    })
+  }, [])
+
+  function handleSelectActivity(id: string)
+  {
+    setSelectedActivity(activities.find(x => x.id === id))
+  }
+
+  function handleCancelSelectActivity()
+  {
+    setSelectedActivity(undefined);
+  }
+
+  function handleFormOpen(id?: string)
+  {
+    id ? handleSelectActivity(id): handleCancelSelectActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose() {
+    setEditMode(false);
+  }
+
+  function handleCreateOrEditActivity(activity: Activity) {
+    activity.id ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
+    : setActivities()
+  }
+
+  return (
+    <> /*same as Fragment*/
+      <NavBar openForm={handleFormOpen} />
+      <Container style={{marginTop: '7em'}}>
+        <ActivityDashboard activities={activities}
+        selectedActivity={selectedActivity}
+        selectActivity={handleSelectActivity}
+        cancelSelectActivity={handleCancelSelectActivity}
+        editMode={editMode}
+        openForm={handleFormOpen}
+        closeForm={handleFormClose}
+          />
+      </Container>
+    </>
+  );
+}
+
+export default App;
