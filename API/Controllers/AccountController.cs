326c1738540355d82d15aa174007d8f69789b12d
aurimasjurgelis <dev.aurimas@gmail.com>
2021-05-14 20:17:01 +0300

    login done

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -48,10 +48,12 @@ namespace API.Controllers
         {
             if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
             {
-                return BadRequest("Email taken");
+                ModelState.AddModelError("email", "Email taken");
+                return ValidationProblem();
             }
             if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.Username))
             {
+                ModelState.AddModelError("username", "Username taken");
                 return BadRequest("Username taken");
             }
 
