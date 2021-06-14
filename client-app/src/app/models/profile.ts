ffc776456dbf3ce17b99cd258fe99c6a5c2df666
aurimasjurgelis <dev.aurimas@gmail.com>
2021-06-14 22:00:19 +0300

    setting up for remote

---
+++ client-app/src/app/models/profile.ts
@@ -0,0 +1,14 @@
+export interface Profile {
+    username: string;
+    displayName: string;
+    image?: string;
+    bio?: string; 
+}
+
+export class Profile implements Profile {
+    constructor(user: User) {
+        this.username = user.username;
+        this.displayName = user.displayName;
+        this.image = user.image;
+    }
+}
